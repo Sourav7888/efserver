1b71a93a70df81b4f4383f9ed7b5e760d3e3e9bf
tantelynj <razafindrabe.tantely@gmail.com>
2021-10-14 11:58:29 -0400

    refractor permission

--- core/permissions.py
+++ core/permissions.py
@@ -19,54 +19,70 @@ def get_or_create_user_info(request) -> UserInfo:
 
 class CheckRequestBody(BasePermission):
     """
-    This permission check the request body keyword
+    This permission check the request body or keyword
     This only check a single instance ie: Division: exampleDivision and not Division: [Division1, ...]
     This catches whether the queried instance exist or not as well so that will be handled by a 403
     When using this permission the request body to check division and facility must be
     facility_name and division_name
+
+    If user_info has not been created as well this will handle it
     """
 
     def has_permission(self, request, view):
         types = ["POST", "HEAD", "DELETE", "GET", "PUT", "PATCH"]
-        data = [getattr(request, x) for x in types if getattr(request, x, None)]
+        body = getattr(request, request.method, None)
 
         # Check that the user has UserInfo already created if not create
         if not hasattr(request.user, "user_info"):
             get_or_create_user_info(request)
 
         # Check the request body
-        if data:
-            if "division_name" in data[0]:
-                try:
-                    _division = Division.objects.get(
-                        division_name=data[0]["division_name"]
-                    )
-                except Division.DoesNotExist:
-                    return False
+        if body:
+            if not self.has_division_permission(request, body):
+                return False
 
-                if request.user.user_info.customer != _division.customer:
-                    return False
-
-            if "facility_name" in data[0]:
-                try:
-                    facility = Facility.objects.get(
-                        facility_name=data[0]["facility_name"]
-                    )
-
-                    # Check if the facility is of the same customer base
-                    if request.user.user_info.customer != facility.division.customer:
-                        return False
-
-                    # Otherwise check this user
-                    # Check the user access
-                    if request.user.user_info.access_level == "RESTRICTED":
-                        access_control = FacilityAccessControl.objects.filter(
-                            user=request.user, facility=facility
-                        )
-                        if not access_control.exists():
-                            return False
-
-                except (Facility.DoesNotExist, FacilityAccessControl.DoesNotExist):
-                    return False
+            if not self.has_facility_permission(request, body):
+                return False
 
         return True
+
+    def has_division_permission(self, request, body) -> bool:
+        """
+        Check division permission if in body
+        """
+        if "division_name" in body:
+            try:
+                _division = Division.objects.get(division_name=body["division_name"])
+            except Division.DoesNotExist:
+                return False
+
+            if request.user.user_info.customer != _division.customer:
+                return False
+
+        return True
+
+    def has_facility_permission(self, request, body) -> bool:
+        """
+        Check facility permission if in body
+        """
+        if "facility_name" in body:
+            try:
+                facility = Facility.objects.get(facility_name=body["facility_name"])
+
+                # Check if the facility is of the same customer base
+                if request.user.user_info.customer != facility.division.customer:
+                    return False
+
+                # Otherwise check this user
+                # Check the user access
+                if request.user.user_info.access_level == "RESTRICTED":
+                    access_control = FacilityAccessControl.objects.filter(
+                        user=request.user, facility=facility
+                    )
+                    if not access_control.exists():
+                        return False
+
+            except (Facility.DoesNotExist, FacilityAccessControl.DoesNotExist):
+                return False
+
+            return True
