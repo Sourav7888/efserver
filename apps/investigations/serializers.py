be557bf9d2488b22772da8ceb18bd7f96ba2c313
tantelynj <razafindrabe.tantely@gmail.com>
2021-10-26 15:24:26 -0400

    added check to avoid repeating code

--- apps/investigations/serializers.py
+++ apps/investigations/serializers.py
@@ -27,6 +27,48 @@ class CreateInvestigationSr(serializers.ModelSerializer):
 
 
 class UpdateInvestigationSr(serializers.ModelSerializer):
+    def check_context(self, instance, context):
+        """
+        Check and assign an investigator if no one is assigned to
+        the investigation
+        """
+        request = context["request"]
+        investigator = instance.investigation_investigator_id
+        user = request.user.user_info
+
+        if investigator is None:
+            instance.investigation_investigator_id = user
+            instance.save()
+            return True
+
+        elif investigator == user.user_name:
+            return True
+
+        return False
+
+    def update(self, instance, validated_data):
+        # can only update if the current user is the investigator of
+        # the investigation
+        if self.check_context(instance, self.context):
+            return super().update(instance, validated_data)
+
+        raise serializers.ValidationError(
+            {"message": "You are not allowed to perform this action!"}
+        )
+
+    def partial_update(self, instance, validated_data):
+        """
+        When asking for a patch only the investigator gets updated
+        """
+        # can only update if the current user is the investigator of
+        # the investigation
+        if self.check_context(instance, self.context):
+            return super().update(instance, validated_data)
+
+        raise serializers.ValidationError(
+            {"message": "You are not allowed to perform this action!"}
+        )
+
     class Meta:
         model = Investigation
         fields = [
