cf1ff86772730058a650c8e9e92908f66e75088b
tantelynj <razafindrabe.tantely@gmail.com>
2021-10-28 12:22:57 -0400

    added prod settings

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: python manage.py migrate
 web: gunicorn backend.wsgi --log-file -
 worker: celery -A server worker --loglevel=info --concurrency=4 --max-tasks-per-child=1 --without-heartbeat --without-gossip --without-mingle
--- server/settings/prod.py
+++ server/settings/prod.py
@@ -4,6 +4,46 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SECRET_KEY = os.environ.get("PRODUCTION_SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["efworkstation-server.herokuapp.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PRODUCTION_DATABASE_NAME"),
+        "USER": os.environ.get("PRODUCTION_DATABASE_USER"),
+        "PASSWORD": os.environ.get("PRODUCTION_DATABASE_PASSWORD"),
+        "HOST": os.environ.get("PRODUCTION_DATABASE_HOST"),
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    },
+}
+
+STATIC_URL = "/static/"
+
+# << S3 STATIC BUCKETS CONFIG >>
+AWS_DEFAULT_ACL = "public-read"
+AWS_QUERYSTRING_AUTH = False
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+
+AWS_ACCESS_KEY_ID = os.environ.get("PRODUCTION_AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("PRODUCTION_AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("PRODUCTION_AWS_STORAGE_BUCKET_NAME")
+
+
+CELERY_BROKER_URL = os.environ.get("CLOUDAMQP_URL")
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_IGNORE_RESULT = True
+
 # Auth0 config
 AUTH0_AUDIENCE = os.getenv("PRODUCTION_AUTH0_AUDIENCE")
 AUTH0_DOMAIN = os.getenv("PRODUCTION_AUTH0_DOMAIN")
