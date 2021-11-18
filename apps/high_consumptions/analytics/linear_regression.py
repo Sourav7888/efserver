4ed9037dec8438e6d2342dd824abf47e73e2978a
tantelynj <razafindrabe.tantely@gmail.com>
2021-11-18 12:23:32 -0500

    removed unecessary import

--- apps/high_consumptions/analytics/linear_regression.py
+++ apps/high_consumptions/analytics/linear_regression.py
@@ -1,11 +1,7 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-import numpy as np
-from sklearn import preprocessing
-from matplotlib import pyplot as plt
 
 # @TODO: test
-def get_linear_regression_model(x, y, mse=False, include_test=False, include_og=False):
+def get_linear_regression_model(x, y, include_test=False, include_og=False):
     """
     get linear regression model, return the model and the mean squared error
     """
