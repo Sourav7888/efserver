c44a8a2ea0c110ca72aabedff0bd8c9b1487bc38
tantelynj <razafindrabe.tantely@gmail.com>
2021-11-26 11:44:37 -0500

    added token auth

--- server/settings/base.py
+++ server/settings/base.py
@@ -71,6 +71,7 @@ REST_FRAMEWORK = {
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.CursorPagination",
