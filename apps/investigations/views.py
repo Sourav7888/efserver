5bc0917ddfd2e4507a192703b89b96b4eb4f23de
tantelynj <razafindrabe.tantely@gmail.com>
2022-01-06 22:21:40 -0500

    update

--- apps/investigations/views.py
+++ apps/investigations/views.py
@@ -1,6 +1,4 @@
-from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsInvestigationManager, IsInvestigator, HasInvestigationAccess
 from .serializers import (
