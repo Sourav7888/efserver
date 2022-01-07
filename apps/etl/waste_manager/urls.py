4a5883e40ae08b42c7aaff5dd598be3b037ed16c
tantelynj <razafindrabe.tantely@gmail.com>
2022-01-06 22:52:12 -0500

    update

--- apps/etl/waste_manager/urls.py
+++ apps/etl/waste_manager/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 
-patterns = []
+patterns = [path("", views.test.as_view(), name="waste_manager_test")]
 
 urlpatterns = [path("", include(patterns))]
--- apps/etl/waste_manager/views.py
+++ apps/etl/waste_manager/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class test(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"})
+
