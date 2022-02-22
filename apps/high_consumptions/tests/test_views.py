336714937a5ee5feaa2e9d85ba7a2a196c789c51
tantelynj <razafindrabe.tantely@gmail.com>
2022-02-22 18:41:24 -0500

    update

--- apps/high_consumptions/tests/test_views.py
+++ apps/high_consumptions/tests/test_views.py
@@ -25,30 +25,30 @@ class ViewsTestCase(BaseTest):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_generate_hc_by_facility(self):
+    def test_generate_hc_by_facility(self):
 
-    #     user = auth.get_user(self.client)
-    #     UserInfo.objects.create(user=user)
+        user = auth.get_user(self.client)
+        UserInfo.objects.create(user=user)
 
-    #     inv_auth = InvestigationAuthorization.objects.create(user_info=user.user_info)
+        inv_auth = InvestigationAuthorization.objects.create(user_info=user.user_info)
 
-    #     inv_auth.is_investigator = True
-    #     inv_auth.access_investigation = True
-    #     inv_auth.is_investigation_manager = True
+        inv_auth.is_investigator = True
+        inv_auth.access_investigation = True
+        inv_auth.is_investigation_manager = True
 
-    #     inv_auth.save()
+        inv_auth.save()
 
-    #     url = reverse("generate-hc-report-by-facility")
-    #     response = self.client.get(
-    #         url,
-    #         data={
-    #             "facility": "CoreFacilityName",
-    #             "utility_type": "electricity",
-    #             "investigation_date": "2020-01-01",
-    #         },
-    #     )
+        url = reverse("generate-hc-report-by-facility")
+        response = self.client.get(
+            url,
+            data={
+                "facility": "CoreFacilityName",
+                "utility_type": "electricity",
+                "investigation_date": "2020-01-01",
+            },
+        )
 
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_generate_hc_by_division(self):
         user = auth.get_user(self.client)
