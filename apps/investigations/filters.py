4074cd7042957376c763c90b80f638c11a23b64d
tantelynj <razafindrabe.tantely@gmail.com>
2022-03-15 18:33:45 -0400

    added more functionalities to investigations

--- apps/investigations/filters.py
+++ apps/investigations/filters.py
@@ -6,6 +6,11 @@ class GetInvestigationsFl(filters.FilterSet):
     open_investigation = filters.BooleanFilter(
         field_name="investigation_investigator", lookup_expr="isnull"
     )
+
+    open_bas_fix = filters.BooleanFilter(
+        field_name="investigation_tech", lookup_expr="isnull"
+    )
+
     # field not supported
     investigation_document = filters.CharFilter(
         field_name="investigation_document", lookup_expr="icontains"
